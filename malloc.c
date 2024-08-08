7a27b4768b38b51ca389483fae6689fd7b1a7fe7
sapph <sapphire.alysa.star@gmail.com>
2024-08-08 09:35:39 -0700

    free vs delete example in cpp

---
+++ malloc.c
@@ -0,0 +1,12 @@
+// Why do we need malloc and free?
+
+// On Windows, the typical maximum size for a stack is 1MB, whereas it is 8MB on a typical modern Linux, although those values are adjustable in various ways. 
+// If the sum of your stack variables (including low-level overhead such as return addresses, stack-based arguments, return value placeholders and alignment bytes) 
+// in the entire call stack exceeds that limit, you get a stack overflow, which typically takes down your program without any chance at recovery.
+
+
+int main() {
+
+    int *x = malloc(sizeof(int));
+    *x = 10;
+}
